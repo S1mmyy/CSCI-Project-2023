c2d73d491f84dd6ce53ea75febb6ada0f6e24489
aidan <aidan.ledgewood@gmail.com>
2023-04-07 00:01:17 -0400

    test hopefully this saves to only my branch

--- src/main/java/MinecraftMath/demo/MainApplication.java
+++ src/main/java/MinecraftMath/demo/MainApplication.java
@@ -10,12 +10,35 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("Title.fxml"));
+        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("LandingPage.fxml"));
+        FXMLLoader loader3 = new FXMLLoader(getClass().getResource("hello-view.fxml"));
 
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("LandingPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        // Load root elements of each scene
+        Parent root1 = loader1.load();
+        Parent root2 = loader2.load();
+        Parent root3 = loader3.load();
+
+        // Get controllers for each scene
+        Scene1Controller controller1 = loader1.getController();
+        Scene2Controller controller2 = loader2.getController();
+        Scene3Controller controller3 = loader3.getController();
+
+        // Create scenes
+        Scene scene1 = new Scene(root1);
+        Scene scene2 = new Scene(root2);
+        Scene scene3 = new Scene(root3);
+
+        // Set controllers for each scene
+        controller1.setScene(scene2);
+        controller2.setScene(scene3);
+        controller3.setScene(scene1);
+
+        // Set initial scene
+        primaryStage.setScene(scene1);
+
+        // Show stage
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
