af064c18259044b1f26761dc51a4efb7be7199bd
Erick <erick.brandao.lopes@gmail.com>
2023-04-11 12:49:43 -0400

    Custom font working. All you need to do to set it to other labels is to add the style arguments

--- src/main/java/MinecraftMath/demo/MainApplication.java
+++ src/main/java/MinecraftMath/demo/MainApplication.java
@@ -1,19 +1,27 @@
 package MinecraftMath.demo;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("LandingPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Minecraft Math!");
+        Font.loadFont(MainApplication.class.getResource("MinecraftFont.otf").toExternalForm(), 10);
+
+
         stage.setScene(scene);
         stage.show();
     }
