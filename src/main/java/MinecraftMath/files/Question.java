57f67be11e4e6cdad212894af24079dd477b17af
Erick <erick.brandao.lopes@gmail.com>
2023-04-27 16:27:53 -0400

    Implementation of TRUE-FALSE type of questions and Multiple Choice.

--- src/main/java/MinecraftMath/files/Question.java
+++ src/main/java/MinecraftMath/files/Question.java
@@ -5,9 +5,21 @@ public class Question {
     static char grade;
     static int difficultyLevel = 0;
 
-    static String questionType = "mc";
+
+    //Possible values, "mc" for Multiple Choice.   "tf" for True or False.
+    static String questionType = "tf";
+
+    static String isTrue = "True";
+
+    public String getIsTrue(){
+        return isTrue;
+    }
     static String answer;
 
+    public String getQuestionType(){
+
+        return questionType;
+    }
     public Question() {
 
     }
