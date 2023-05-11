6bf47a628c08d3d5003ef4686aeddf9e8abee78b
Erick <erick.brandao.lopes@gmail.com>
2023-05-11 10:54:56 -0400

    Rewards and Improvements

--- src/main/java/MinecraftMath/files/ResultsController.java
+++ src/main/java/MinecraftMath/files/ResultsController.java
@@ -5,21 +5,39 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ResultsController extends MasterController {
+    @FXML
+    private Label resultsText;
+
+    @FXML
+    private Button snakeButton;
+    @FXML
+    private ImageView eggImage;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         loadUserDataOntoScene();
+
+        resultsText.setText("Results! \nYou received a total score of "+userHolder.getUser().getGradeScore()+"%");
+
+        if(userHolder.getUser().getGradeScore() > 85){
+
+            snakeButton.setVisible(true);
+            eggImage.setVisible(true);
+
+        }
     }
 
     @FXML
     private Button landingButton;
-    @FXML
-    private Button snakeButton;
+
     @FXML
     private void openSnake() throws Exception {
         Snake snake = new Snake();
